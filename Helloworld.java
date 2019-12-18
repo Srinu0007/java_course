4e4a7b2f9127f353fb7038157d078f3fb3c4dc97
Your Name <polamarasettysrinu007@gmail.com>
2019-12-18 19:51:06 +0530

    first push

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+public class Helloworld{
+public static void main(String[] args)
+{
+	System.out.println("helloworld");
+	int myfirstnumber = (10 + 12) * (12+ 12);
+	int mysecondnumber = 12;
+	int mytotal = myfirstnumber + mysecondnumber ;
+	System.out.println(mytotal);
+	int mylastone = 1000 - mytotal ;
+	System.out.println(mylastone);
+}
+}
