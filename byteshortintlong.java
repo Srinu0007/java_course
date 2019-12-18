e18dd38314b030957ae48da0aad712f361196989
Your Name <polamarasettysrinu007@gmail.com>
2019-12-18 21:40:57 +0530

    modified

--- byteshortintlong.java
+++ byteshortintlong.java
@@ -7,5 +7,16 @@ public class byteshortintlong{
 		System.out.println("Integer min value = "+mymaxintvalue);
 		System.out.println("busted max value = " + (mymaxintvalue + 1));
 		System.out.println("busted min value = " + (myminintvalue - 1));
+		int mymaxinttest = 2_147_148_647;
+		byte myminbytevalue = Byte.MIN_VALUE;
+		byte mymaxbytevalue = Byte.MAX_VALUE;
+		System.out.println("byte max value = " + (mymaxbytevalue + 1));
+                System.out.println("byte min value = " + (myminbytevalue - 1));
+                short myminshortvalue = Short.MIN_VALUE;
+                short mymaxshortvalue = Short.MAX_VALUE;
+                System.out.println("short max value = " + (mymaxshortvalue + 1));
+                System.out.println("short min value = " + (myminshortvalue - 1));
+
+
 	}}
 		
