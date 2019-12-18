7e37cd362debf1a521de37ff51d5f0061bd0c994
Your Name <polamarasettysrinu007@gmail.com>
2019-12-18 21:21:51 +0530

    Insert

---
+++ byteshortintlong.java
@@ -0,0 +1,11 @@
+public class byteshortintlong{
+	public static void main(String[] args){
+		int myvalue = 10000;
+		int myminintvalue = Integer.MIN_VALUE;
+		int mymaxintvalue = Integer.MAX_VALUE;
+		System.out.println("Integer max value = "+myminintvalue);
+		System.out.println("Integer min value = "+mymaxintvalue);
+		System.out.println("busted max value = " + (mymaxintvalue + 1));
+		System.out.println("busted min value = " + (myminintvalue - 1));
+	}}
+		
